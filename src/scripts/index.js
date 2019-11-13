be499a2022e2058fead84bb4bef525ee4aa2a9c4
LymanAri <lyman.ari@gmail.com>
2019-11-13 16:36:28 +0200

    mathOperations

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,45 @@
-import '../styles/index.scss';
+import "../styles/index.scss";
 
-console.log('webpack starterkit');
+const a = 20;
+console.log("Const = ", a);
+
+let b = 5;
+console.log("let = ", b);
+
+const sum = a + b;
+console.log(sum);
+
+const sub = a - b;
+console.log(sub);
+
+const mult = a * b;
+console.log(mult);
+
+const div = a / b;
+console.log(div);
+
+const frac = a % b;
+console.log(frac);
+
+
+
+
+
+
+//console.log("camelCase");
+//console.log("kebab-case");
+
+//console.log("PascalCase");
+//console.log("snake_case");
+
+//console.log("Desktop ==> Electron");
+//console.log("Smartphone & Tablet ==> Cordova");
+//console.log("Server ==> Node.js");
+//console.log("---History---");
+//console.info("1995 - Netscape");
+//console.info("1997 - ECMA Standart");
+//console.log("Ecma Script = JavaScript");
+//console.log("1999 - EcmaScript 3");
+//console.log("2009 - ECMAScript 5 ==> ES5");
+//console.log("2015 - ECMAScript 2015 ==> ES6");
+//console.log(">2015 - new releases");
